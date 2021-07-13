4e72a61cb9de9b24768912ef73706a63a6cc3dae
karpovska <oksanachv@gmail.com>
2021-07-13 09:57:34 +0200

    Setup for mars rover project. Packages installed

--- further_with_funcational_js/implement_immutablejs.js
+++ further_with_funcational_js/implement_immutablejs.js
@@ -4,14 +4,29 @@
 // The original code is provided below, how might you re-write both the store object and updateStore function with Immutable?
 // Remember if you need help, the docs should be the first place you look.
 
-let store = {
+/*let store = {
     user: {
         first_name: 'John',
         last_name: 'Doe'
     }
-}
+}*/
+
+/**/
+
+const Immutable = require('immutable');
+
+const store = Immutable.Map({
+    user: Immutable.Map({
+        first_name: 'John',
+        last_name: 'Doe'
+    })
+})
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
     render(root, store)
 }
+
+//newState = 'name';
+updateStore(store, newState);
+console.log(store.getIn(['user', 'name']));
